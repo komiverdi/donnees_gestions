1e096e534446a525ae8e466ea795e273c00b0ba4
leslie547 <leslieouedraogo29@gmail.com>
2026-05-07 10:42:41 +0000

    modification apportée par leslie

---
+++ main.py
@@ -0,0 +1,30 @@
+try:
+            # Séparer les éléments
+            parties = entree.split()
+            nombre1 = float(parties[0])
+            operateur = parties[1]
+            nombre2 = float(parties[2])
+
+            # Calculer
+            if operateur == "+":
+                resultat = nombre1 + nombre2
+            elif operateur == "-":
+                resultat = nombre1 - nombre2
+            elif operateur == "*":
+                resultat = nombre1 * nombre2
+            elif operateur == "/":
+                if nombre2 == 0:
+                    print("Erreur : division par zéro !\n")
+                    continue
+                resultat = nombre1 / nombre2
+            else:
+                print("Opérateur inconnu. Utilisez + - * /\n")
+                continue
+
+            print(f"Résultat : {nombre1} {operateur} {nombre2} = {resultat}\n")
+
+        except (IndexError, ValueError):
+            print("Format invalide. Exemple correct : 5 + 3\n")
+
+# Lancer la calculatrice
+calculatrice()
